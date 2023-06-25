51b91b93d696c145b9780fce8397efa174bf9f0f
Loren M. Kerr <loren@lorenkerr.com>
2023-06-25 12:27:08 -0600

    one last style

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -5,12 +5,10 @@ const Error = styled.div<{ $inputColor?: string }>`
   border: 1px solid transparent;
   padding: 0.6em 1.2em;
   margin: 0.6em 0;
-  font-size: 1.4em;
+  font-size: 1em;
   font-weight: 500;
   font-family: inherit;
   background-color: #500000;
-  cursor: pointer;
-  transition: border-color 0.25s;
   color: ${(props) => props.$inputColor || '#ababab'};
 `;
 
